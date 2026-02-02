79562a2d614a23571bb654eed5508dbb22c96591
Mariam <mariam@gmail.com>
2026-02-02 10:41:18 +0200

    Add project files.

---
+++ Albashiti_Mariam_25_26_ProjetC#ProgEven/Form2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Albashiti_Mariam_25_26_ProjetC_ProgEven
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+    }
+}
