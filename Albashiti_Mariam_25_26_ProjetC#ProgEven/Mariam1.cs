8e76be484281dbaf151aae47d62f97240c5a8801
Mariam <mariam@gmail.com>
2026-02-02 12:32:48 +0200

    Ajouter form2 changer le nom du fichier et la classe

---
+++ Albashiti_Mariam_25_26_ProjetC#ProgEven/Mariam1.cs
@@ -0,0 +1,15 @@
+namespace Albashiti_Mariam_25_26_ProjetC_ProgEven
+{
+    public partial class Mariam1 : Form
+    {
+        public Mariam1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- Albashiti_Mariam_25_26_ProjetC#ProgEven/Program.cs
+++ Albashiti_Mariam_25_26_ProjetC#ProgEven/Program.cs
@@ -11,7 +11,7 @@ namespace Albashiti_Mariam_25_26_ProjetC_ProgEven
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new Mariam1());
         }
     }
 }
