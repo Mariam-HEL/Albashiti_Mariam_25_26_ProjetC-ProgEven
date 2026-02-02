bc57d7580ad16ab4a6c7db09996519e7aab21107
Mariam <mariam@gmail.com>
2026-02-02 10:41:18 +0200

    Add project files.

---
+++ Albashiti_Mariam_25_26_ProjetC#ProgEven/Form1.cs
@@ -0,0 +1,10 @@
+namespace Albashiti_Mariam_25_26_ProjetC_ProgEven
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
