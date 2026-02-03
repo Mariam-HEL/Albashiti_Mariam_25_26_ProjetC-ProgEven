87f108679019a92503b225bf4a58971563809d8e
Mariam <mariam@gmail.com>
2026-02-03 11:15:08 +0200

    Le travail 1 jusqu'a la partie PictureBox(c'est Fait).

--- Albashiti_Mariam_25_26_ProjetC#ProgEven/Mariam1.Designer.cs
+++ Albashiti_Mariam_25_26_ProjetC#ProgEven/Mariam1.Designer.cs
@@ -28,32 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
+            btnName = new Button();
+            lblTest = new Label();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
-            // button1
+            // btnName
             // 
-            button1.Location = new Point(364, 27);
-            button1.Name = "button1";
-            button1.Size = new Size(94, 29);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
+            btnName.Anchor = AnchorStyles.None;
+            btnName.AutoSize = true;
+            btnName.Cursor = Cursors.Hand;
+            btnName.Font = new Font("Showcard Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnName.ForeColor = Color.OliveDrab;
+            btnName.Location = new Point(254, 36);
+            btnName.Name = "btnName";
+            btnName.Size = new Size(200, 40);
+            btnName.TabIndex = 0;
+            btnName.Text = "Maryoma";
+            btnName.UseVisualStyleBackColor = true;
+            btnName.Click += button1_Click;
             // 
-            // Form1
+            // lblTest
+            // 
+            lblTest.Anchor = AnchorStyles.None;
+            lblTest.AutoSize = true;
+            lblTest.BackColor = SystemColors.ActiveCaption;
+            lblTest.FlatStyle = FlatStyle.Flat;
+            lblTest.ForeColor = Color.IndianRed;
+            lblTest.Location = new Point(12, 36);
+            lblTest.Name = "lblTest";
+            lblTest.Size = new Size(199, 20);
+            lblTest.TabIndex = 1;
+            lblTest.Text = "Mariam ALBASHITI B2Q2 IS2";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources._9b4fc1daa920440a8c03e8d9ae105bdf;
+            pictureBox1.ImageLocation = "";
+            pictureBox1.Location = new Point(254, 127);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(411, 295);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // Mariam1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(button1);
-            Name = "Form1";
-            Text = "Form1";
+            BackColor = SystemColors.Control;
+            ClientSize = new Size(934, 489);
+            Controls.Add(pictureBox1);
+            Controls.Add(lblTest);
+            Controls.Add(btnName);
+            Cursor = Cursors.Cross;
+            Name = "Mariam1";
+            Text = "Mon application agreable ";
+            Load += Mariam1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private Button button1;
+        private Button btnName;
+        private Label lblTest;
+        private PictureBox pictureBox1;
     }
 }
--- Albashiti_Mariam_25_26_ProjetC#ProgEven/Mariam1.cs
+++ Albashiti_Mariam_25_26_ProjetC#ProgEven/Mariam1.cs
@@ -5,11 +5,27 @@ namespace Albashiti_Mariam_25_26_ProjetC_ProgEven
         public Mariam1()
         {
             InitializeComponent();
+            this.Size = new Size(800, 600);
+            this.MaximizeBox = false;
+            this.MaximizeBox = false;
+            lblTest.AutoSize = true;
+            lblTest.Size = new Size(200, 50);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void Mariam1_Load(object sender, EventArgs e)
+        {
+            lblTest.AutoSize = true;
+            lblTest.Size = new Size(200, 50);
+            lblTest.Location = new Point(50, 30);
+            int largeurGroupe = Math.Max(lblTest.Width, btnName.Width);
+
+            lblTest.Left = (this.ClientSize.Width - lblTest.Width) / 2;
+            btnName.Left = (this.ClientSize.Width - btnName.Width) / 2;
         }
     }
 }
